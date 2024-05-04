a16161146a46b497ff420594554caa0fae27a627
Hyane Moussassa <hyane.moussassa1@gmail.com>
2024-05-05 00:23:00 +0100

    complete signup page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,10 +3,11 @@ import bcryptjs from "bcryptjs";
 
 
 export const signup = async (req, res, next) => {
+    console.log(req.body);
     const { username, email, password } = req.body;
-    const hash = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hash });
     try {
+        const hash = bcryptjs.hashSync(password, 10);
+        const newUser = new User({ username, email, password: hash });
         await newUser.save();
         res.status(201).json({ message: "User signup successful" });
     } catch (error) {

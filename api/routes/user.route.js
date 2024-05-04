37639303b1c2b2e760dbbad05ab0e60c34a565fc
Hyane Moussassa <hyane.moussassa1@gmail.com>
2024-05-04 15:59:03 +0100

    create a test api route

---
+++ api/routes/user.route.js
@@ -0,0 +1,9 @@
+import express from "express";
+import userController from "../controllers/user.controller.js";
+
+
+const userRouter = express.Router();
+
+userRouter.get("/test", userController);
+
+export default userRouter;

50665392426aaf1a4bae99bae6d44be1b26a7743
Hyane Moussassa <hyane.moussassa1@gmail.com>
2024-05-06 18:16:30 +0100

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,35 +1,91 @@
-import React from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { ref, getStorage, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { app } from '../firebase'
 
 export default function Profile() {
+  const fileRef = useRef();
   const currentUser = useSelector(state => state.user.currentUser);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  console.log(filePerc)
+  console.log(fileUploadError)
+  console.log(formData)
+
+
+  useEffect(() => {
+    if (file){
+      handleFileUpload(file);
+    }
+  }, [file])
+
+  const handleFileUpload = async (file) => {
+    setFileUploadError(false);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setFilePerc(Math.round(progress));
+    },
+    (error) => {
+      setFileUploadError(true);
+    },
+    ()=> {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setFormData({...formData, avatar: downloadURL});
+      });
+    });
+  }
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form src="" alt="" className='flex flex-col gap-4'>
+        <input 
+          type="file" 
+          onChange={(e) => setFile(e.target.files[0])}
+          ref={fileRef} hidden accept='image/*'/>
         <img 
-          src={currentUser.avatar} 
+          src={formData.avatar || currentUser.avatar} 
+          onClick={()=> fileRef.current.click()}
           className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
-          alt="" />
-          <input 
-            type="text" 
-            id = 'username'
-            placeholder='username' 
-            className = 'border p-3 rounded-lg'
-          />
-          <input 
-            type="text" 
-            id = 'email'
-            placeholder='email' 
-            className = 'border p-3 rounded-lg'
-          />
-          <input 
-            type="text" 
-            id = 'password'
-            placeholder='password' 
-            className = 'border p-3 rounded-lg'
-          />
-          <button className='bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80'>update</button>
+          alt="" 
+        />
+        <p className='text-sm self-center'>
+          {fileUploadError ? 
+            (<span className='text-red-700'>Error uploading file (image must be less than 2mb)</span>) 
+            : filePerc > 0 && filePerc < 100 ? 
+              (<span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>) : 
+                filePerc === 100 && fileUploadError == false ?
+                (<span className='text-green-700'>Image uploaded successfully</span>)
+                : ('')           
+          }
+        </p>
+        <input 
+          type="text" 
+          id = 'username'
+          placeholder='username' 
+          className = 'border p-3 rounded-lg'
+        />
+        <input 
+          type="text" 
+          id = 'email'
+          placeholder='email' 
+          className = 'border p-3 rounded-lg'
+        />
+        <input 
+          type="text" 
+          id = 'password'
+          placeholder='password' 
+          className = 'border p-3 rounded-lg'
+        />
+        <button className='bg-slate-700 text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80'>update</button>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer'>Delete account</span>
